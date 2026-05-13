f1bf48857e1981361e5dc3177299a068b3b7e681
unknown <muktinathrajbanshi786@gmail.com>
2026-05-13 21:43:19 +0545

    aja lai yati yar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,30 @@
+import { Toaster } from "react-hot-toast"
+import { Route, Routes } from "react-router-dom"
+import Login from "./pages/Login"
+import AppLayout from "./pages/AppLayout"
+import Home from "./pages/Home"
+import Products from "./pages/Products"
+import ProductPage from "./pages/ProductPage"
 
 const App = () => {
   return (
-    <div>
-      App
-    </div>
+    <>
+      <Toaster position="top-right" toastOptions={{duration: 3000, style: {
+        background: "#1B3022", color: "#fff", borderRadius: "12px", fontSize:
+        "14px" }}} />
+
+        <Routes>
+          {/* Auth pages - No Navbar/Footer  */}
+          <Route path="/login" element={<Login />} />
+          {/* Main pages - With Navbar/Footer  */}
+          <Route path="/" element={<AppLayout />}>
+            <Route index element={<Home />} />
+            <Route path="products" element={<Products />} />
+            <Route path="products:id" element={<ProductPage />} />
+          </Route>
+        </Routes>
+
+    </>
   )
 }
 
