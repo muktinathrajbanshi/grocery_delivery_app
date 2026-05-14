aef44285df592dfffd061ff698dcec244c6005cf
unknown <muktinathrajbanshi786@gmail.com>
2026-05-14 20:11:04 +0545

    chalo khana khane

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,5 +1,20 @@
+import { useState } from "react"
 
 const Login = () => {
+
+  const [isLoginState, setIsLoginState] = useState(true)
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [loading, setLoading] = useState(false)
+
+
+  const handleSubmit = async (e: React.SubmitEvent) => {
+      e.preventDefault()
+      setLoading(true);
+      setTimeout(() => window.location.href = "/", 1000)
+  }
+
+
   return (
     <div>
       Login
