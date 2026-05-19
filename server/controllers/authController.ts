f2ceae5b7a2ead78ce7ef830758a2539de37a53a
unknown <muktinathrajbanshi786@gmail.com>
2026-05-19 23:45:11 +0545

    aja lai yati yar

--- server/controllers/authController.ts
+++ server/controllers/authController.ts
@@ -1,5 +1,12 @@
 import { Request, Response } from "express";
 import { prisma } from "../config/prisma.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+// Generate JWT token
+const generateToken = (id: string) => {
+    return jwt.sign({id}, )
+}
 
 // Register 
 // POST /api/auth/register
@@ -11,4 +18,14 @@ export const register = async (req:Request, res: Response) => {
     }
 
     const existingUser = await prisma.user.findUnique({ where: {email: email.toLowerCase()}})
+
+    if(existingUser) {
+        return res.status(400).json({ message: "User already exists with this email" })
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    const user = await prisma.user.create({
+        data: {name, email: email.toLowerCase(), password: hashedPassword}
+    })
 }
