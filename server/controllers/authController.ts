6f57a4e719079133c759f41ed150c604b813350a
unknown <muktinathrajbanshi786@gmail.com>
2026-05-19 23:37:15 +0545

    ram ram k saro yar

--- server/controllers/authController.ts
+++ server/controllers/authController.ts
@@ -1,7 +1,14 @@
-import { Request } from "express";
+import { Request, Response } from "express";
+import { prisma } from "../config/prisma.js";
 
 // Register 
 // POST /api/auth/register
-export const register = async (req:Request) => {
-    
+export const register = async (req:Request, res: Response) => {
+    const {name, email, password} = req.body;
+
+    if(!name || !email || !password) {
+        return res.status(400).json({ message: "Please provide all fields" })
+    }
+
+    const existingUser = await prisma.user.findUnique({ where: {email: email.toLowerCase()}})
 }
