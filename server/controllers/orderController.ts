8f8a039e97a28cfd7a862a9e552a6f7cd875bd08
unknown <muktinathrajbanshi786@gmail.com>
2026-05-20 21:29:36 +0545

    nidra lago my lord

--- server/controllers/orderController.ts
+++ server/controllers/orderController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { prisma } from "../config/prisma.js";
+import { timeStamp } from "node:console";
 
 // Create Order
 // POST /api/orders
@@ -100,5 +101,51 @@ export const getUserOrders = async (req: Request, res: Response) => {
 // Get single order
 // GET /api/orders/:id
 export const getOrder = async (req: Request, res: Response) => {
-    
+    const order = await prisma.order.findFirst({
+        where: {id: req.params.id as string, userId: req.user!.id},
+        include: {deliveryPartner: {select: {name: true, phone: true, avatar: true, vehicleType: true}}}
+    })
+
+    if(!order) {
+        return res.status(401).json({ message: "Order not found" });
+    }
+    res.json({order})
+}
+
+// Update order status (admin)
+// PUT /api/orders/:id/status
+export const updateOrderStatus = async (req: Request, res: Response) => {
+    const { status, note } = req.body;
+    const order = await prisma.order.findUnique({where: {id: req.params.id as string}})
+
+    if(!order) {
+        return res.status(401).json({ message: "Order not found" });
+    }
+
+    const history = (Array.isArray(order.statusHistory) ? order.statusHistory : []) as any[];
+    history.push({status, note: note || `Order ${status.toLowerCase()}`, 
+    timeStamp: new Date()})
+
+    const updateOrder = await prisma.order.update({
+        where: {id: req.params.id as string},
+        data: {status, statusHistory: history}
+    })
+
+    res.json({order: updateOrder})
+}
+
+// Get all orders (admin)
+// GET /api/orders/all
+export const getAllOrders = async (req: Request, res: Response) => {
+
+    const orders = await prisma.order.findMany({
+        where: {NOT: [{paymentMethod: "card", isPaid: false}]},
+        include: {
+            user: {select: {name: true, email: true}},
+            deliveryPartner: {select: {name: true, phone: true, email: true}}
+        },
+        orderBy: { createdAt: "desc" },
+    })
+
+    res.json({orders})
 }
