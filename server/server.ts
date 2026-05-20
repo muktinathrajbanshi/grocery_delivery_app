3bb1b2fb8117d835bb94a19520cfabe7c3054dd9
unknown <muktinathrajbanshi786@gmail.com>
2026-05-20 21:44:29 +0545

    la aja lai yati yar

--- server/server.ts
+++ server/server.ts
@@ -2,6 +2,10 @@ import "dotenv/config";
 import express, { NextFunction, Request, Response } from 'express';
 import cors from "cors";
 import authRouter from "./routes/authRoutes.js";
+import productRouter from "./routes/productRoutes.js";
+import uploadRouter from "./routes/uploadRoutes.js";
+import orderRouter from "./routes/orderRoutes.js";
+
 
 const app = express();
 
@@ -16,6 +20,9 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 app.use("/api/auth", authRouter)
+app.use("/api/products", productRouter)
+app.use("/api/upload", uploadRouter)
+app.use("/api/orders", orderRouter)
 
 // Error handling
 app.use((error: any, req: Request, res: Response, next: NextFunction) => {
