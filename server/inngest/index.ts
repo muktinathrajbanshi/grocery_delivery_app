555da2dba4286b773bce79bcdd9e517d84fbdcb7
unknown <muktinathrajbanshi786@gmail.com>
2026-05-21 10:11:14 +0545

    let's get started muktinath

---
+++ server/inngest/index.ts
@@ -0,0 +1,37 @@
+import { Inngest } from "inngest";
+import { prisma } from "../config/prisma.js";
+
+const LOW_STOCK_THRESHOLD = 10;
+
+// Create a client to send and receive events
+export const inngest = new Inngest({ id: "grocery-delivery-app" });
+
+// Low Stock Alert to Admin Email
+const checkLowStock = inngest.createFunction(
+  { id: "check-low-stock",
+    name: "Low Stock Alert",
+    triggers: [{ event: "inventory/stock.updated" }] },
+  async ({ event, step }) => {
+    const {productId} = event.data;
+
+    const product = await step.run("fetch-product", async () => {
+        return await prisma.product.findUnique({where: {id: productId}})
+    })
+
+    if(!product || product.stock === null || product.stock >= LOW_STOCK_THRESHOLD) {
+        return {skipped: true, stock: product?.stock}
+    }
+
+    await step.run("send-low-stock-email", async () => {
+        const adminEmails = process.env.ADMIN_EMAILS 
+        ? process.env.ADMIN_EMAILS.split(",").map((e) => e.trim())
+        : [];
+
+        if(adminEmails.length === 0) return {skipped: true, reason: "No admin emails"};
+
+        
+    })
+  },
+);
+
+export const functions = [];
--- server/server.ts
+++ server/server.ts
@@ -5,7 +5,8 @@ import authRouter from "./routes/authRoutes.js";
 import productRouter from "./routes/productRoutes.js";
 import uploadRouter from "./routes/uploadRoutes.js";
 import orderRouter from "./routes/orderRoutes.js";
-
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js"
 
 const app = express();
 
@@ -23,6 +24,7 @@ app.use("/api/auth", authRouter)
 app.use("/api/products", productRouter)
 app.use("/api/upload", uploadRouter)
 app.use("/api/orders", orderRouter)
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 // Error handling
 app.use((error: any, req: Request, res: Response, next: NextFunction) => {
