823cc49d9568f6b072b33a994a5bab7c9d9792d0
unknown <muktinathrajbanshi786@gmail.com>
2026-05-21 22:18:06 +0545

    la kaile saknu yar

--- server/controllers/addressController.ts
+++ server/controllers/addressController.ts
@@ -13,3 +13,51 @@ export const getAddresses = async (req:Request, res: Response) => {
 
 // Add address
 // POST /api/addresses
+export const addAddress = async (req:Request, res: Response) => {
+    const {label, address, city, state, zip, isDefault, lat, lng} = req.body;
+
+    // Require coordinates
+    if(lat == null || lng == null){
+        return res.status(400).json({ message: "Location coordinates are required. Please allow location access." });
+    }
+
+    const currentAddresses = await prisma.address.findMany({
+        where: {userId: req.user!.id},
+    })
+
+    let makeDefault = isDefault;
+    if(currentAddresses.length === 0) makeDefault = true;
+
+    if(makeDefault) {
+        await prisma.address.updateMany({
+            where: {userId: req.user!.id},
+            data: {isDefault: false}
+        })
+    }
+
+    await prisma.address.create({
+        data: {
+            userId: req.user!.id,
+            label,
+            address,
+            city,
+            state,
+            zip,
+            isDefault: makeDefault,
+            lat: Number(lat),
+            lng: Number(lng)
+        }
+    })
+
+    const addresses = await prisma.address.findMany({
+        where: {userId: req.user!.id},
+        orderBy: {createdAt: "asc"}
+    })
+    res.status(201).json({addAddress})
+}
+
+// Update address
+// PUT /api/addresses/:id
+export const updateAddress = async (req:Request, res: Response) => {
+
+}
