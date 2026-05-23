f9fb1cbc6f3cd926bb2a53141e3de11454e13b61
unknown <muktinathrajbanshi786@gmail.com>
2026-05-23 12:06:39 +0545

    yo chi feri birse chhu save garna

--- server/controllers/orderController.ts
+++ server/controllers/orderController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { prisma } from "../config/prisma.js";
-import { timeStamp } from "node:console";
+import { inngest } from "../inngest/index.js";
 
 // Create Order
 // POST /api/orders
@@ -73,6 +73,13 @@ export const createOrder = async (req: Request, res: Response) => {
             data: {stock: {decrement: item.quantity}}
         })
     }
+
+    // Send stock update events for each product in the order
+    for(const item of orderItems) {
+        await inngest.send({name: "inventory/stock.updated", data: {productId: item.product}})
+    }
+
+    await inngest.send({name: "order/placed", data: {orderId: order.id}})
 }
 
 // Get user's orders
