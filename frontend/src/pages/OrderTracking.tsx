c21a9ae678d94ecc75b4ff947631cb6e9acfeb5c
unknown <muktinathrajbanshi786@gmail.com>
2026-05-24 17:03:46 +0545

    feri challenges aayo yar updated nai vayena

--- frontend/src/pages/OrderTracking.tsx
+++ frontend/src/pages/OrderTracking.tsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom"
 import type { Order } from "../types";
-import { dummyDashboardOrdersData } from "../assets/assets";
 import Loading from "../components/Loading";
 import { ArrowLeftIcon, MapPinIcon, PhoneIcon } from "lucide-react";
 import OrderOTP from "../components/OrderTracking/OrderOTP";
 import LiveMap from "../components/OrderTracking/LiveMap";
 import OrderTimeLine from "../components/OrderTracking/OrderTimeLine";
+import api from "../config/api";
 
 
 const OrderTracking = () => {
@@ -19,8 +19,7 @@ const OrderTracking = () => {
   const [liveLocation, setLiveLocation] = useState<{lat: number; lng: number} | null>(null)
 
   useEffect(() => {
-    setOrder(dummyDashboardOrdersData.find((o) => o.id === id) as any)
-    setLoading(false)
+    api.get(`/orders/${id}`).then((res) => setOrder(res.data.order)).catch(() => navigate("/orders")).finally(() => setLoading(false))
   }, [id, navigate])
 
   if(loading) return <Loading />
